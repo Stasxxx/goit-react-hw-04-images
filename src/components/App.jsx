75e60e4abe5a5c307fb671a5ab45dd0715115e93
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-10-06 22:12:57 +0300

    add API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import axios from 'axios';
+import { Searchbar } from './Searchbar/Searchbar';
+
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
+const KEY = '29487133-26ae31273c20ec953386c6e64';
+// axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_API_KEY;
+export class App extends Component {
+  state = {
+    images: [],
+  }
+
+  async componentDidMount() {
+  try {
+    const response = await axios.get(`/?key=${KEY}&q=cat&page=1&image_type=photo&orientation=horizontal&per_page=12`)
+  } catch (error) {
+    
+  }
+}
+
+  render() {
+    return (
+      <>
+      <Searchbar />
+      </>
+    );
+    
+  }
+  
+}
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,20 @@
+
+export const Searchbar = () => {
+    return (
+        <header className="searchbar">
+            <form className="form">
+                <button type="submit" className="button">
+                <span className="button-label">Search</span>
+                </button>
+
+                <input
+                className="input"
+                type="text"
+                // autocomplete="off"
+                // autofocus
+                placeholder="Search images and photos"
+                />
+            </form>
+        </header>
+    )
+}
