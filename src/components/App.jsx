6a57939dd28bf53acd1dff17585f462e6c5ba1ca
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-10-07 22:10:38 +0300

    Submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -10,11 +12,12 @@ const KEY = '29487133-26ae31273c20ec953386c6e64';
 export class App extends Component {
   state = {
     images: [],
+    imageName: '',
   }
 
   async componentDidMount() {
   try {
-    const response = await axios.get(`/?key=${KEY}&q=cat&page=1&image_type=photo&orientation=horizontal&per_page=12`)
+    const response = await axios.get(`/?key=${KEY}&q=${this.state.imageName}&page=1&image_type=photo&orientation=horizontal&per_page=12`)
     // console.log(response.data)
     this.setState({
       images: [...response.data.hits],
@@ -22,13 +25,23 @@ export class App extends Component {
   } catch (error) {
     
   }
-}
+  };
+
+  handleFormSubmit = imageName => {
+    console.log(imageName)
+    if (this.state.imageName.trim() === '') {
+      toast.error('Введіть імя картинки')
+      return
+    }
+    this.setState({ imageName });
+  };
 
   render() {
     return (
       <>
-        <Searchbar />
+        <Searchbar onSubmit={this.handleFormSubmit} />
         <ImageGallery images={this.state.images} />
+        <ToastContainer autoClose={2500} position="top-center"/>
       </>
     );
     
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,21 +1,45 @@
-import { SearchbarHeader,SearchForm, SearchFormButton, SearchFormButtonLabel, SearchFormInput } from "./Searchbar.styled";
+import { Component } from "react";
+import { FcSearch } from "react-icons/fc";
+import { SearchbarHeader, SearchForm, SearchFormButton, SearchFormButtonLabel, SearchFormInput } from "./Searchbar.styled";
 
-export const Searchbar = () => {
-    return (
-        <SearchbarHeader className="searchbar">
-            <SearchForm className="form">
+export class Searchbar extends Component{
+    state = {
+        imageName: '',
+    }
+
+    handleNameChange = e => {
+        // console.log(e.currentTarget.value)
+        this.setState({imageName: e.currentTarget.value.toLowerCase()})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        this.props.onSubmit(this.state.imageName)
+        this.setState({imageName: ''})
+    }
+
+    render() {
+        return (
+        <SearchbarHeader>
+            <SearchForm onSubmit={this.handleSubmit}>
                 <SearchFormButton type="submit" className="button">
+                    <FcSearch/>
                 <SearchFormButtonLabel className="button-label">Search</SearchFormButtonLabel>
                 </SearchFormButton>
 
                 <SearchFormInput
                 className="input"
                 type="text"
+                value={this.state.imageName}
                 autocomplete="off"
                 autoFocus
                 placeholder="Search images and photos"
+                onChange={this.handleNameChange}    
                 />
             </SearchForm>
         </SearchbarHeader>
     )
+    }
+    
 }
